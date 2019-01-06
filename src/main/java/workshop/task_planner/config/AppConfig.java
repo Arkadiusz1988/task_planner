d99c374781a3a826ac8559e554a240a839b48364
Arkadiusz Hojenski <arkadiusz.hojenski@gmail.com>
2019-01-06 10:24:27 +0100

    first commit

---
+++ src/main/java/workshop/task_planner/config/AppConfig.java
@@ -0,0 +1,18 @@
+package workshop.task_planner.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+}
